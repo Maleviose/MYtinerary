48c6800df132f8e4194d2b6b2f6f5e282eed9421
Malevi <martinlei96@hotmail.com>
2019-11-04 23:44:34 -0300

    Epic 3 hecho, estados y componentes OK

--- Sprint1/my-app/src/App.js
+++ Sprint1/my-app/src/App.js
@@ -1,23 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/images/logo.svg';
+import './assets/css/App.css';
+import header from './component/header.js'
+import Header from './component/header.js';
 
 class App extends Component {
+    constructor(arg){
+        super(arg)
+        this.state = {
+            counter: 0,
+        }
+    }
+    sumar(n){
+        this.setState({
+            counter: this.state.counter+1
+        })
+    }
+    restar(n){
+        this.setState({
+            counter: this.state.counter-1
+        })
+    }
+
     render() {
-        return ( <
-            div className = "App" >
-            <
-            div className = "App-header" >
-            <
-            img src = { logo }
-            className = "App-logo"
-            alt = "logo" / >
-            <
-            h2 > Welcome to React < /h2> </div > <
-            p className = "App-intro" >
-            To get started, edit < code > src / App.js < /code> and save to reload. < /
-            p > <
-            /div>
+        return ( 
+        <div className = "App">
+            <div className = "App-header" >
+                <img src = { logo }className = "App-logo"alt = "logo" />
+                <h2 > Welcome to React </h2> 
+            </div> 
+        <p className = "App-intro">
+            To get started, edit <code > src / App.js </code> and save to reload. </p> 
+            
+            <span>contador: {this.state.counter}</span>
+            <button onClick={this.sumar.bind(this)}>+</button>
+            <button onClick={this.restar.bind(this)}>-</button>
+            <Header></Header>
+        </div>
         );
     }
 }
---
+++ Sprint1/my-app/src/component/header.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Header extends Component{
+    render(){
+        return (
+            <div>
+                <h1>Hola soy el header</h1>
+                <h2>probando 1</h2>
+                <h3>probando 2</h3>
+            </div>
+            );
+    }
+}
+export default Header;
+
+
