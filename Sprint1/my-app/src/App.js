acb2665245e9be941df31881d289acdcaddefbf1
Diego Weinmann <diego.weinmann@hotmail.com>
2019-11-06 09:33:57 -0300

    Imagenes subidos

--- Sprint1/my-app/src/App.js
+++ Sprint1/my-app/src/App.js
@@ -1,42 +1,41 @@
 import React, { Component } from 'react';
 import logo from './assets/images/logo.svg';
 import './assets/css/App.css';
-import header from './component/header.js'
 import Header from './component/header.js';
 
 class App extends Component {
-    constructor(arg){
+    constructor(arg) {
         super(arg)
         this.state = {
             counter: 0,
         }
     }
-    sumar(n){
+    sumar(n) {
         this.setState({
-            counter: this.state.counter+1
+            counter: this.state.counter + 1
         })
     }
-    restar(n){
+    restar(n) {
         this.setState({
-            counter: this.state.counter-1
+            counter: this.state.counter - 1
         })
     }
 
     render() {
-        return ( 
-        <div className = "App">
-            <div className = "App-header" >
-                <img src = { logo }className = "App-logo"alt = "logo" />
-                <h2 > Welcome to React </h2> 
-            </div> 
-        <p className = "App-intro">
-            To get started, edit <code > src / App.js </code> and save to reload. </p> 
-            
-            <span>contador: {this.state.counter}</span>
-            <button onClick={this.sumar.bind(this)}>+</button>
-            <button onClick={this.restar.bind(this)}>-</button>
-            <Header></Header>
-        </div>
+        return (
+            <div className="App">
+                <div className="App-header" >
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2 > Welcome to React </h2>
+                </div>
+                <p className="App-intro">
+                    To get started, edit <code > src / App.js </code> and save to reload. </p>
+
+                <span>contador: {this.state.counter}</span>
+                <button onClick={this.sumar.bind(this)}>+</button>
+                <button onClick={this.restar.bind(this)}>-</button>
+                <Header></Header>
+            </div>
         );
     }
 }
--- Sprint1/my-app/src/index.js
+++ Sprint1/my-app/src/index.js
@@ -4,6 +4,6 @@ import App from './App';
 import './assets/css/index.css';
 
 ReactDOM.render(
-  <App/>,
+  <App />,
   document.getElementById('root')
 );
