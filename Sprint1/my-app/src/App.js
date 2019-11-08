d37b689e6f1e84566de550b5ac0ddd1ad720703b
Martin <maleviose>
2019-11-08 10:51:26 -0300

    Todo funcionando, agregando routes

--- Sprint1/my-app/src/App.js
+++ Sprint1/my-app/src/App.js
@@ -1,44 +1,35 @@
 //Cambiar Credenciales en windows para git!!!!
 // Route https://www.youtube.com/watch?v=oWEys5cX3kE
-import React, { Component } from 'react';
-
-import './assets/css/App.css';
-import logoMI from './assets/images/MYtineraryLogo.png'
-import start from './assets/images/circled-right-2.png'
-import home from './assets/images/homeIcon.png'
- //Agrego Bootstrap con "npm install react-bootstrap bootstrap"
- //Agrego React router con "npm install -- save react-router"
- import { BrowseRouter as Router,Switch, Route} from 'react-router-dom';
-
-import logo from './assets/images/logo.svg';
-import Header from './component/header.js';
-
-const login = () => <h1>log in</h1>
-const createacc = () => <h1>create acc</h1>
+import React, { Component } from "react";
+import "./assets/css/App.css";
+//Agrego Bootstrap con "npm install react-bootstrap bootstrap"
+//Agrego React router con "npm install -- save react-router"
+// import { BrowseRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./component/header.js";
 
 class App extends Component {
-    // constructor(arg) {
-    //     super(arg)
-    //     this.state = {
-    //         counter: 0,
-    //     }
-    // }
-    // sumar(n) {
-    //     this.setState({
-    //         counter: this.state.counter + 1
-    //     })
-    // }
-    // restar(n) {
-    //     this.setState({
-    //         counter: this.state.counter - 1
-    //     })
-    // }
+  // constructor(arg) {
+  //     super(arg)
+  //     this.state = {
+  //         counter: 0,
+  //     }
+  // }
+  // sumar(n) {
+  //     this.setState({
+  //         counter: this.state.counter + 1
+  //     })
+  // }
+  // restar(n) {
+  //     this.setState({
+  //         counter: this.state.counter - 1
+  //     })
+  // }
 
-    render() {
-        return (
-            <div className="App">
-                {/******************************** CODIGO DE PRUEBA PROPS Y ESTADOS!! Epic 3, ponele *********************************/}
-                {/* <div className="App-header" >
+  render() {
+    return (
+      <div className="App">
+        {/******************************** CODIGO DE PRUEBA PROPS Y ESTADOS!! Epic 3, ponele *********************************/}
+        {/* <div className="App-header" >
                     <img src={logo} className="App-logo" alt="logo" />
                     <h2 > Welcome to React </h2>
                 </div>
@@ -50,29 +41,18 @@ class App extends Component {
                 <button onClick={this.restar.bind(this)}>-</button>
                 <Header></Header> */}
 
-                {/*********** Aca empiezo el landing page!!!! ***********/}
-                <div>
-                    <img src = {logoMI} className="logoSized" alt = "logo"></img>
-                    <div>
-                        <h3>Find your perfect trip, designed by insider who know and love their cities.</h3>
-                        <h2>Start browsing</h2>
-                    </div>
-                    <link> <img src = {start} alt ="flechita" className="startImg"></img></link>
-                    
-                    <h3>Want to build your own MYtinerary?</h3>
-                    
-                        {/* <Router>{/*No me funca bootstrap */}
-                        {/* <Switch>
-                            <Route path="/log-in" Component={login}>Log in </Route>
-                            <Route path="/create-acc" Component={createacc}>Create Account </Route>
-                        </Switch>
-                        </Router> */}
-                    
-                    <img src = {home} alt = "home" className="homeImg"></img>
-                </div>
-            </div>
-        );
-    }
+        {/*********** Aca empiezo el landing page!!!! ***********/}
+        <Header>
+          {/* <Router>
+            {/* <Switch>
+                <Route path="/log-in" Component={login}>Log in </Route>
+                <Route path="/create-acc" Component={createacc}>Create Account </Route>
+            </Switch>
+            </Router> */}
+        </Header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- Sprint1/my-app/src/component/header.js
+++ Sprint1/my-app/src/component/header.js
@@ -1,16 +1,26 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import logoMI from "../assets/images/MYtineraryLogo.png";
+import start from "../assets/images/circled-right-2.png";
+import home from "../assets/images/homeIcon.png";
 
-class Header extends Component{
-    render(){
-        return (
-            <div>
-                <h1>Hola soy el header</h1>
-                <h2>probando 1</h2>
-                <h3>probando 2</h3>
-            </div>
-            );
-    }
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <img src={logoMI} className="logoSized" alt="logo"></img>
+        <div>
+          <h3>Find your perfect trip, designed by insider who know and love their cities.</h3>
+          <h2>Start browsing</h2>
+        </div>
+        <div>
+          <img src={start} alt="flechita" className="startImg"></img>
+        </div>
+
+        <h3>Want to build your own MYtinerary?</h3>
+
+        <img src={home} alt="home" className="homeImg"></img>
+      </div>
+    );
+  }
 }
 export default Header;
-
-
