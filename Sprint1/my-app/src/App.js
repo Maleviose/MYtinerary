cbc69113f58ee1156cb0ba42e41337612d473f51
Malevi <martinlei96@hotmail.com>
2019-11-27 11:53:20 -0300

    Render de ciudades funcionando

--- Sprint1/my-app/src/App.js
+++ Sprint1/my-app/src/App.js
@@ -1,7 +1,7 @@
 //Cambiar Credenciales en windows para git!!!!
 // Route https://www.youtube.com/watch?v=oWEys5cX3kE
 import React, { Component } from "react";
-import { BrowserRouter, Route} from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 //Agrego Bootstrap con "npm install react-bootstrap bootstrap"
 //Agrego React router con "npm install -- save react-router"
 // import { BrowseRouter as Router, Switch, Route } from "react-router-dom";
@@ -15,54 +15,53 @@ import Create from "./component/create.jsx";
 
 
 class App extends Component {
-  // constructor(arg) {
-  //     super(arg)
-  //     this.state = {
-  //         counter: 0,
-  //     }
-  // }
-  // sumar(n) {
-  //     this.setState({
-  //         counter: this.state.counter + 1
-  //     })
-  // }
-  // restar(n) {
-  //     this.setState({
-  //         counter: this.state.counter - 1
-  //     })
-  // }
+    // constructor(arg) {
+    //     super(arg)
+    //     this.state = {
+    //         counter: 0,
+    //     }
+    // }
+    // sumar(n) {
+    //     this.setState({
+    //         counter: this.state.counter + 1
+    //     })
+    // }
+    // restar(n) {
+    //     this.setState({
+    //         counter: this.state.counter - 1
+    //     })
+    // }
 
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="App">
-        {/******************************** CODIGO DE PRUEBA PROPS Y ESTADOS!! Epic 3, ponele *********************************/}
-        {/* <div className="App-header" >
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h2 > Welcome to React </h2>
-                </div>
-                <p className="App-intro">
-                    To get started, edit <code > src / App.js </code> and save to reload. </p>
-
-                <span>contador: {this.state.counter}</span>
-                <button onClick={this.sumar.bind(this)}>+</button>
-                <button onClick={this.restar.bind(this)}>-</button>
-                <Header></Header> */}
-
-
-
-        {/*********** Aca empiezo el landing page!!!! ***********/}
-          <Header></Header>
-        
-            <Route exact={true} path="/" component={Content} />
-            <Route path='/cities' component={City}/>
-            <Route path='/login' component={Login}/>
-            <Route path='/signup' component={Create}/>
-         <Footer></Footer>
-        </div>
-        </BrowserRouter>
-    );
-  }
+    render() {
+        return ( 
+          <BrowserRouter>
+          <div className="App">
+          {/******************************** CODIGO DE PRUEBA PROPS Y ESTADOS!! Epic 3, ponele *********************************/}
+          {/* <div className="App-header" >
+                      <img src={logo} className="App-logo" alt="logo" />
+                      <h2 > Welcome to React </h2>
+                  </div>
+                  <p className="App-intro">
+                      To get started, edit <code > src / App.js </code> and save to reload. </p>
+                  <span>contador: {this.state.counter}</span>
+                  <button onClick={this.sumar.bind(this)}>+</button>
+                  <button onClick={this.restar.bind(this)}>-</button>
+                  <Header></Header> */}
+  
+  
+  
+          {/*********** Aca empiezo el landing page!!!! ***********/}
+            <Header></Header>
+          
+              <Route exact={true} path="/" component={Content} />
+              <Route path='/cities' component={City}/>
+              <Route path='/login' component={Login}/>
+              <Route path='/signup' component={Create}/>
+           <Footer></Footer>
+          </div>
+          </BrowserRouter>
+        );
+    }
 }
 
 export default App;
--- Sprint1/my-app/src/component/city.jsx
+++ Sprint1/my-app/src/component/city.jsx
@@ -1,13 +1,56 @@
 import React, { Component } from "react";
+import axios from "axios"
 import "../assets/css/App.css";
 
 export default class City extends Component {
-  render() {
-    return (
-      <div>
-        <h3>Soy el city</h3>
-      </div>
-      
-    );
-  }
+state={listaPaises:[]}
+
+async componentDidMount(){
+  const res = await axios.get("http://localhost:4000/api/cities");
+  this.setState({listaPaises:res.data.respuesta});
 }
+
+render(){
+  return(
+    <div>
+    
+      { <ul>
+      {this.state.listaPaises.map((elem, i)=>{return<button><li key={i}>{elem.pais}{" - "}{elem.ciudad}</li></button>})}
+      </ul>}
+
+       
+    </div>
+    
+  );
+}
+}
+
+// fetch("http://localhost:4000/api/cities", {
+//         method: "GET", //estructura del JSON
+//       }).then(function (response) { //Una vez que termine esto hace esto
+//         if (response.ok)
+//             return response.json();
+//         throw new Error(response.statusText);
+//       }).then(function (json) {
+//        console.log(json);
+//         data = json.respuesta;
+//         PasandoAlista(data)
+//       })
+
+
+//       function PasandoAlista(cosas) {
+//         let res = [];
+//         for(let i =0; i<cosas.length; i++){
+//           res.push(<li>+cosas[i].ciudad + " " +cosas[i].pais + </li>);
+//           }
+//           return <ul>res</ul>;
+//         }
+        
+// console.log(data)
+// export default class City extends Component {
+//   render() {
+//     return (
+//         <div></div>
+//     );
+//   }
+// }
